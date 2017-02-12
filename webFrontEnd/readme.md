5599acc799a62e4a32ede49e85db71c72feabb27
loren klingman <loren@klingman.us>
2017-02-12 11:39:12 -0500

    loaded up some sample code for an angular app

---
+++ webFrontEnd/readme.md
@@ -0,0 +1,11 @@
+# Web Front End (Team 6)
+
+To run this code, it would be useful to use http-server-angular.
+
+First, you will need to have npm installed on your computer.
+
+Then, run ``npm install`` in the project folder.
+
+Next, run ``./node_modules/.bin/angular-http-server``.
+
+Now, launch your web browser to ``http://127.0.0.1:8080``.
---
+++ webFrontEnd/test/protractor.js
@@ -0,0 +1,16 @@
+// Template: https://docs.angularjs.org/api/ngRoute/service/$route
+
+it('should load and compile correct template', function() {
+    element(by.linkText('Moby: Ch1')).click();
+    var content = element(by.css('[ng-view]')).getText();
+    expect(content).toMatch(/controller: ChapterController/);
+    expect(content).toMatch(/Book Id: Moby/);
+    expect(content).toMatch(/Chapter Id: 1/);
+
+    element(by.partialLinkText('Scarlet')).click();
+
+    content = element(by.css('[ng-view]')).getText();
+    expect(content).toMatch(/controller: BookController/);
+    expect(content).toMatch(/Book Id: Scarlet/);
+});
+
