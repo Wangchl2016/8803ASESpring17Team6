28c00e270e921304b91d484c533f0764fb5e4163
loren klingman <loren@klingman.us>
2017-03-15 15:43:04 -0400

    add cors

--- js/login.controller.js
+++ js/login.controller.js
@@ -1,7 +1,24 @@
 angular.module('playlistModule')
 
-    .controller('LoginController', function($scope, $route, $routeParams, $location) {
-        $scope.$route = $route;
-        $scope.$location = $location;
-        $scope.$routeParams = $routeParams;
+    .controller('LoginController', function($scope, $http, $rootScope) {
+        $scope.login = function (loginForm) {
+            if (!loginForm.$valid) {
+                alert("Please enter a valid email address and password.");
+            }
+            $http({
+                method: 'GET',
+                url: $rootScope.baseURL+'/api/users/login?email='+encodeURIComponent($scope.email)+
+                    '&password='+encodeURIComponent($scope.password)
+            }).then(function successCallback(response, status) {
+                if (response.hasOwnProperty('token')) {
+                    alert("Valid! Got a token! "+response.token);
+                } else {
+                    alert("Login Failed");
+                }
+            }, function errorCallback(response, status) {
+                alert("Login Failed "+status);
+            });
+            alert($scope.email+" "+$scope.password);
+            return false;
+        };
     });
