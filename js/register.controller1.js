18890ee6c1684aef57c8674fdd4103e76eea0586
szhang440 <342407043@qq.com>
2017-03-25 17:31:06 -0400

    add register

--- js/register.controller1.js
+++ js/register.controller1.js
@@ -1,3 +1,27 @@
 /**
  * Created by Poul on 3/25/17.
  */
+
+angular.module('playlistModule')
+
+    .controller('RegisterController1', function($scope, $http, $rootScope) {
+        $scope.register = function (registerForm) {
+            if (!registerForm.$valid) {
+                alert("Please enter valid name, email address and password.");
+                return false;
+            }
+            $http({
+                method: 'POST',
+                url: $rootScope.baseURL+'/api/users',
+                data: registerForm
+            }).then(function successCallback(response, status) {
+                console.log(response);
+                alert("server reply: "+response.data);
+            }, function errorCallback(response) {
+                alert("Register Failed " + response.status);
+            });
+
+            alert("local echo: "+$scope.email+" "+$scope.password+" "+$scope.firstname +" "+$scope.lastname);
+            return false;
+        };
+    });
